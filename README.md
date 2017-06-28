84cb6914d234a4f97ea9f4b0705659ea3c68e80b
nathan <nathan@spitfire.zswap.net>
2017-06-28 19:25:46 -0400

    specify world name via hiera, generate new maps

--- README.md
+++ README.md
@@ -26,7 +26,18 @@ This module will install a Factorio headless server.
 
 Simply `include factorio` on the nodes and/or hierarchies you wish to have a server setup
 
+The default world name will be generic. If you'd like to give your world a specific name, 
+you can do so via hiera: 
+`factorio::world_name: 'yourcustomworldname'`
+
+## Generating a new world / Changing worlds
+
+Follow the steps below to change to a different save game:
+* Stop factorio (systemctl stop factorio)
+* Update factorio::world_name with the changed/new name you wish to use
+* Server will change maps and restart on next puppet run
+
 ## Limitations
 
-To create a new world, you must manually delete the existing save file. The next puppet run
+To create a new world, you must stop the running server, then manually delete the existing save file. The next puppet run
 will then setup a fresh world. 
--- manifests/install.pp
+++ manifests/install.pp
@@ -1,4 +1,6 @@
-class factorio::install {
+class factorio::install (
+    $world_name = hiera(factorio::world_name, world1)
+    ) {
     $required_packages = [ 'wget', 'unzip' ]
     package { $required_packages:
         ensure => installed,
@@ -13,9 +15,16 @@ class factorio::install {
         cwd     => '/home/factorio',
         unless  => '/bin/test -d /home/factorio/factorio',
         }
+    file { '/home/factorio/ensure_permissions.py':
+	ensure   => file,
+	owner    => 'factorio',
+        group 	 => 'factorio',
+	mode     => '0644',
+	source	 => 'puppet:///modules/factorio/ensure_permissions.py'
+	}
     exec { 'factorio_chown':
         command  => '/bin/chown -R factorio:factorio /home/factorio',
-        unless   => '/bin/ls -ld /home/factorio/factorio | /bin/grep "factorio factorio"',
+        unless   => '/bin/python /home/factorio/ensure_permissions.py',
         }
     file { '/etc/systemd/system/factorio.service':
         ensure  => file,
@@ -25,12 +34,13 @@ class factorio::install {
         content  => template('factorio/factorio.service.erb')
         }
     exec { 'create_server':
-        command => '/home/factorio/factorio/bin/x64/factorio --create /home/factorio/factorio/saves/world.zip',
-        unless  => '/bin/test -f /home/factorio/factorio/saves/world.zip',
+        command => "/home/factorio/factorio/bin/x64/factorio --create /home/factorio/factorio/saves/$world_name.zip",
+        unless  => "/bin/test -f /home/factorio/factorio/saves/$world_name.zip",
         }
     exec { 'reload_systemd':
-        command => '/bin/systemctl daemon-reload',
+        command     => '/bin/systemctl daemon-reload',
         refreshonly => true,
-        subscribe => File['/etc/systemd/system/factorio.service'],
+        subscribe   => File['/etc/systemd/system/factorio.service'],
+	notify      => Service['factorio_service']
         }
 }
